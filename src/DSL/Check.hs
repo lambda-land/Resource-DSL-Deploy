cd4144bd079519826c9cb8ece746b895fadd8180
Eric Walkingshaw <eric@walkingshaw.net>
2016-04-04 09:21:13 +0000

    Use schemas in environments.
    git-svn-id: https://dsl-external.bbn.com/svn/immortals/trunk/dsl/resource-dsl@262 e2ae056a-197f-4f36-953d-074c54ff641a

--- src/DSL/Check.hs
+++ src/DSL/Check.hs
@@ -16,8 +16,8 @@ import DSL.Type
 -- * Type Checking
 --
 
--- | The typing environment maps variables to refined types.
-type TEnv = Env (Type Refined)
+-- | The typing environment maps variables to refined type schemas.
+type TEnv = Env (Schema Refined)
 
 -- | A monad to support the typing relation. The environment in the
 --   reader monad tracks non-linear assumptions, while the environment
@@ -25,7 +25,7 @@ type TEnv = Env (Type Refined)
 type TypeM = ReaderT TEnv (StateT TEnv (Either String))
 
 -- | Lookup a non-linear assumption.
-lookRef :: Var -> TypeM (Type Refined)
+lookRef :: Var -> TypeM (Schema Refined)
 lookRef v = do
     m <- ask
     case Map.lookup v m of
@@ -33,7 +33,7 @@ lookRef v = do
       _      -> fail ("unbound non-linear variable: " ++ v)
 
 -- | Lookup and consume a linear assumption.
-lookUse :: Var -> TypeM (Type Refined)
+lookUse :: Var -> TypeM (Schema Refined)
 lookUse v = do
     m <- get
     case Map.lookup v m of
@@ -43,14 +43,34 @@ lookUse v = do
 -- | Push a linear assumption onto the typing environment, then a run a typing
 --   computation and check to see whether that assumption was consumed.
 --   If so, return the result. Otherwise, fail.
-typeWith :: Var -> Type Refined -> TypeM a -> TypeM a
-typeWith v t ma = do
+typeWith :: Var -> Schema Refined -> TypeM a -> TypeM a
+typeWith v s ma = do
     old <- gets (Map.lookup v)
-    modify (Map.insert v t)
+    modify (Map.insert v s)
     result <- ma
     unused <- gets (Map.member v)
     when unused (fail ("unused linear variable: " ++ v))
     case old of
-      Just t -> modify (Map.insert v t)
+      Just s -> modify (Map.insert v s)
       _      -> return ()
     return result
+
+-- | Check whether two types are compatible.
+check :: (Eq t, Show t) => String -> Type t -> Type t -> TypeM ()
+check s t u | t == u    = return ()
+            | otherwise = fail ("incompatible types in " ++ s ++ ": "
+                                ++ show t ++ " and " ++ show u)
+
+-- | Print an error message for failed expectation.
+expected :: Show t => String -> Type t -> TypeM a
+expected s t = fail ("expected " ++ s ++ ", got: " ++ show t)
+
+-- | Typing relation. Returns the type schema of a term and the updated
+--   environment or else reports a type error.
+typeOf :: Expr Refined -> TypeM (Schema Refined)
+    -- environment lookup
+typeOf (Ref x)      = lookRef x
+typeOf (Use x)      = lookUse x
+    -- introduction forms
+-- typeOf (Abs x t b)  = typeWith x t (fmap (t :->) (typeOf b))
+-- typeOf (Free e)     = liftM Bang (typeOf e)
