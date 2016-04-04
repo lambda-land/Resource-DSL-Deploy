c9bf5f8bea06d930152f0007e84763427792846f
Eric Walkingshaw <eric@walkingshaw.net>
2016-04-04 09:21:44 +0000

    Factor out linear environment to reuse in semantics.
    git-svn-id: https://dsl-external.bbn.com/svn/immortals/trunk/dsl/resource-dsl@266 e2ae056a-197f-4f36-953d-074c54ff641a

--- src/DSL/Check.hs
+++ src/DSL/Check.hs
@@ -1,7 +1,6 @@
 
 module DSL.Check where
 
-import Control.Monad.Reader
 import Control.Monad.State
 import qualified Data.Map.Strict as Map
 
@@ -16,44 +15,8 @@ import DSL.Type
 -- * Type Checking
 --
 
--- | The typing environment maps variables to refined type schemas.
-type TEnv = Env (Schema Refined)
-
--- | A monad to support the typing relation. The environment in the
---   reader monad tracks non-linear assumptions, while the environment
---   in the state monad tracks linear assumptions.
-type TypeM = ReaderT TEnv (StateT TEnv (Either String))
-
--- | Lookup a non-linear assumption.
-lookRef :: Var -> TypeM (Schema Refined)
-lookRef v = do
-    m <- ask
-    case Map.lookup v m of
-      Just t -> return t
-      _      -> fail ("unbound non-linear variable: " ++ v)
-
--- | Lookup and consume a linear assumption.
-lookUse :: Var -> TypeM (Schema Refined)
-lookUse v = do
-    m <- get
-    case Map.lookup v m of
-      Just t -> put (Map.delete v m) >> return t
-      _      -> fail ("unavailable linear variable: " ++ v)
-
--- | Push a linear assumption onto the typing environment, then a run a typing
---   computation and check to see whether that assumption was consumed.
---   If so, return the result. Otherwise, fail.
-typeWith :: Var -> Schema Refined -> TypeM a -> TypeM a
-typeWith v s ma = do
-    old <- gets (Map.lookup v)
-    modify (Map.insert v s)
-    result <- ma
-    unused <- gets (Map.member v)
-    when unused (fail ("unused linear variable: " ++ v))
-    case old of
-      Just s -> modify (Map.insert v s)
-      _      -> return ()
-    return result
+-- | A monad to support the typing relation.
+type TypeM = EnvM (Schema Refined)
 
 -- | Check whether two types are compatible.
 check :: (Eq t, Show t) => String -> Type t -> Type t -> TypeM ()
--- src/DSL/Env.hs
+++ src/DSL/Env.hs
@@ -1,10 +1,13 @@
 module DSL.Env where
 
-import Data.Set (Set)
-import qualified Data.Set as Set
+import Control.Monad.Reader
+import Control.Monad.State
 
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
+
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 import Data.SBV
 
@@ -20,11 +23,55 @@ type Var = String
 type Env a = Map Var a
 
 -- | Lookup a binding in an environment or fail dynamically if it's not there.
-envLookup :: Var -> Env a -> a
-envLookup v m | Just a <- Map.lookup v m = a
+envLookup :: Var -> Env b -> b
+envLookup v m | Just b <- Map.lookup v m = b
               | otherwise = error $ "Variable not in environment: " ++ v
 
 -- | Generate an environment with fresh symbolic values for each variable.
-symEnv :: (Var -> Symbolic a) -> Set Var -> Symbolic (Env a)
+symEnv :: (Var -> Symbolic b) -> Set Var -> Symbolic (Env b)
 symEnv f s = fmap (Map.fromList . zip vs) (mapM f vs)
   where vs = Set.toList s
+
+
+--
+-- * Linear Environment Monad
+--
+
+-- | The linear environment monad tracks two separate environments: one for
+--   non-linear assumptions, and one for linear assumptions. It's implemented
+--   by a monad transformer stack, structured like so:
+--    * Reader: for non-linear assumptions
+--    * State: for linear assumptions
+--    * Either String: for error reporting
+type EnvM b = ReaderT (Env b) (StateT (Env b) (Either String))
+
+-- | Lookup a non-linear assumption.
+lookRef :: Var -> EnvM b b
+lookRef v = do
+    m <- ask
+    case Map.lookup v m of
+      Just t -> return t
+      _      -> fail ("unbound non-linear variable: " ++ v)
+
+-- | Lookup and consume a linear assumption.
+lookUse :: Var -> EnvM b b
+lookUse v = do
+    m <- get
+    case Map.lookup v m of
+      Just t -> put (Map.delete v m) >> return t
+      _      -> fail ("unavailable linear variable: " ++ v)
+
+-- | Push a linear assumption onto the environment, then run a computation
+--   and check to see whether the assumption was consumed. If so, return the 
+--   result. Otherwise, fail.
+runWith :: Var -> b -> EnvM b a -> EnvM b a
+runWith v b ma = do
+    old <- gets (Map.lookup v)
+    modify (Map.insert v b)
+    result <- ma
+    unused <- gets (Map.member v)
+    when unused (fail ("unused linear variable: " ++ v))
+    case old of
+      Just t -> modify (Map.insert v b)
+      _      -> return ()
+    return result
