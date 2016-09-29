52e6cfabbdb53be6dd6ee31d77961229e1fe9bea
doyougnu <jmy6342@gmail.com>
2016-09-28 20:42:51 -0700

    commiting to checkout branch

--- test/DSL/Test/Effect.hs
+++ test/DSL/Test/Effect.hs
@@ -33,20 +33,24 @@ testCases = zipWith (testCase . show) [1..]
 
 -- ** Tests
 
+testResolveEffect :: TestTree
 testResolveEffect = testGroup "resolveEffect"
     [ testGroup "create" $ testCases
       
       [ do out <- runEffect ["foo"] (Create 3) envEmpty
            envFromList [(["foo"], I 3)] @=? out
       
-      , do out <- runEffect ["foo"] (Create 3) envEmpty >>= runEffect ["bar"] (Create 4)
+      , do out <- runEffect ["foo"] (Create 3) envEmpty
+             >>= runEffect ["bar"] (Create 4)
            envFromList [(["foo"], I 3), (["bar"], I 4)] @=? out
       
-      , do out <- runEffect ["foo"] (Create 3) envEmpty >>= runEffect ["foo","bar"] (Create 4)
+      , do out <- runEffect ["foo"] (Create 3) envEmpty
+             >>= runEffect ["foo","bar"] (Create 4)
            envFromList [(["foo"], I 3), (["foo","bar"], I 4)] @=? out
       
       , assertEffectError ResourceAlreadyExists
-          (runEffect ["foo"] (Create 3) envEmpty >>= runEffect ["foo"] (Create 4))
+          (runEffect ["foo"] (Create 3) envEmpty
+             >>= runEffect ["foo"] (Create 4))
       ]
 
     , testGroup "delete" $ testCases
@@ -54,10 +58,12 @@ testResolveEffect = testGroup "resolveEffect"
       [ do out <- runEffect ["foo"] Delete (envFromList [(["foo"], B True)])
            envEmpty @=? out
       
-      , do out <- runEffect ["foo"] Delete (envFromList [(["foo"], B True), (["bar"], Unit)])
+      , do out <- runEffect ["foo"] Delete (envFromList [(["foo"], B True)
+                                                        , (["bar"], Unit)])
            envFromList [(["bar"], Unit)] @=? out
       
-      , do out <- runEffect ["foo"] Delete (envFromList [(["foo"], B True), (["foo","bar"], Unit)])
+      , do out <- runEffect ["foo"] Delete (envFromList [(["foo"], B True)
+                                                        , (["foo","bar"], Unit)])
            envFromList [(["foo","bar"], Unit)] @=? out
       
       , assertEffectError NoSuchResource
@@ -68,4 +74,16 @@ testResolveEffect = testGroup "resolveEffect"
              >>= runEffect ["foo"] Delete
              >>= runEffect ["foo"] Delete)
       ]
+
+     , testGroup "check" $ testCases
+
+      [ do out <- runEffect ["foo"] (Check (funEq 1729)) envEmpty
+           envFromList [(["foo"], B False)] @=? out
+
+      , do out <- runEffect ["foo"] (Check (funEq 1729)) (envFromList [(["foo"], (I 1729))]) 
+           envFromList [(["foo"], I 1729)] @=? out -- why does this work?
+
+      , assertEffectError NoSuchResource
+           (runEffect ["foo"] (Check (funEq 1)) envEmpty)
+      ] 
     ]
