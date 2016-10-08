6420e1ab3a20e51c17ca1d7eff2d3e6a42c9411e
doyougnu <jmy6342@gmail.com>
2016-10-07 19:37:59 -0700

    finished unary tests

--- test/DSL/Expression/Test.hs
+++ test/DSL/Expression/Test.hs
@@ -19,7 +19,7 @@ runExpr epr env =  fst <$> runInEmptyContext env (evalExpr epr)
 
 -- ** Tests
 testExpression :: TestTree
-testExpression = testGroup "testExpressions"
+testExpression = testGroup "Expressions Tests"
   [ testGroup "evalExpr"
 
     [ testCase "Evaluate a Literal" $
@@ -30,6 +30,46 @@ testExpression = testGroup "testExpressions"
     , testCase "Evaluate a Reference" $
       do out <- runExpr (Ref "x") envEmpty
          let y = Unit
+         y @=? out
+
+    , testCase "Absolute value of a positive num is just the num" $
+      do out <- runExpr (P1 (I_I Abs) (Lit (I 1))) envEmpty
+         let y = I 1
+         y @=? out
+
+    , testCase "Absolute value of a neg num is just the num" $
+      do out <- runExpr (P1 (I_I Abs) (Lit (I (- 1)))) envEmpty
+         let y = I 1
+         y @=? out
+
+    , testCase "Negating a positive creates a negative" $
+      do out <- runExpr (P1 (I_I Neg) (Lit (I 1))) envEmpty
+         let y = I (- 1)
+         y @=? out
+
+    , testCase "Negating a negative creates a negative" $
+      do out <- runExpr (P1 (I_I Neg) (Lit (I (- 1)))) envEmpty
+         let y = I 1
+         y @=? out
+
+    , testCase "Negating zero is zero" $
+      do out <- runExpr (P1 (I_I Neg) (Lit (I 0))) envEmpty
+         let y = I 0
+         y @=? out
+
+    , testCase "signum of zero is zero" $
+      do out <- runExpr (P1 (I_I Sign) (Lit (I 0))) envEmpty
+         let y = I 0
+         y @=? out
+
+    , testCase "signum of a positive is 1" $
+      do out <- runExpr (P1 (I_I Sign) (Lit (I 1729))) envEmpty
+         let y = I 1
+         y @=? out
+
+    , testCase "signum of a negative is -1" $
+      do out <- runExpr (P1 (I_I Sign) (Lit (I (- 1729)))) envEmpty
+         let y = I (-1)
          y @=? out
     ]
   ]
