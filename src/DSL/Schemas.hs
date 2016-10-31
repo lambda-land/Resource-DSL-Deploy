19092c6f7c4dc382f09a4354161bff0a280bf504
doyougnu <jmy6342@gmail.com>
2016-10-30 17:06:46 -0700

    using hjsonschema, instead of json-schema

--- src/DSL/Schemas.hs
+++ src/DSL/Schemas.hs
@@ -12,17 +12,18 @@ import DSL.Primitive
 import DSL.Profile
 import DSL.Resource
 
-import Data.JSON.Schema
+import           Data.Aeson                (Value (..), decode)
+import qualified Data.ByteString.Lazy   as LBS
+import qualified Data.List.NonEmpty     as NE
+import           Data.Maybe                (fromMaybe)
+import qualified Data.JsonSchema.Draft4 as D4
 
 -- Primitives
-instance JSONSchema PType where
-  schema = gSchema
 
-instance JSONSchema PVal where
-  schema = gSchema
-
-instance JSONSchema Param where
-  schema (P pname ptype) = Object
-    [ Field { key = "name", required = True, content = schema $ String (pack pname)}
-    , Field { key = "type", required = True, content = schema ptype}
-    ]
+-- primitives :: IO ()
+primitives = do
+  -- Get Primitives Schemas
+  prims <- LBS.readFile "./json/definitions.json"
+  let schema = fromMaybe (error "Invalid schema JSON.") (decode prims)
+      schemaWithURI = D4.SchemaWithURI schema (Just "./json/configuration-schema.json")
+  return schemaWithURI
