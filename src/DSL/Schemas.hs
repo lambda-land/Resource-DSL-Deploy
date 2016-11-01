8af9b1e461ba6275ba3ac235dab743c3acffc256
doyougnu <jmy6342@gmail.com>
2016-10-31 19:16:31 -0700

    jk using json-schema, need to understand proxies

--- src/DSL/Schemas.hs
+++ src/DSL/Schemas.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PolyKinds #-}
 
 module DSL.Schemas where
 
@@ -12,18 +13,26 @@ import DSL.Primitive
 import DSL.Profile
 import DSL.Resource
 
-import           Data.Aeson                (Value (..), decode)
+import qualified Data.Aeson             as A
 import qualified Data.ByteString.Lazy   as LBS
 import qualified Data.List.NonEmpty     as NE
 import           Data.Maybe                (fromMaybe)
 import qualified Data.JsonSchema.Draft4 as D4
+import           Data.JSON.Schema
 
 -- Primitives
 
--- primitives :: IO ()
-primitives = do
-  -- Get Primitives Schemas
-  prims <- LBS.readFile "./json/definitions.json"
-  let schema = fromMaybe (error "Invalid schema JSON.") (decode prims)
-      schemaWithURI = D4.SchemaWithURI schema (Just "./json/configuration-schema.json")
-  return schemaWithURI
+primitives :: D4.Schema
+primitives = D4.emptySchema { D4._schemaRef = Just "./configuration-schema.json" }
+
+schemaContext :: D4.SchemaWithURI D4.Schema
+schemaContext = D4.SchemaWithURI
+                { D4._swSchema = primitives
+                , D4._swURI    = Just ".json/configuration-schema.json"
+
+                }
+
+instance JSONSchema PType where
+  schema (TUnit :: Proxy) = Constant A.Null
+  schema (TBool) = Boolean
+  schema TInt  = Number unbounded
