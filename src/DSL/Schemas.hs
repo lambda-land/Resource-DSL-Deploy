c968904c0fbda45def9d7bdc905e909c7c56addd
doyougnu <jmy6342@gmail.com>
2016-11-13 11:43:44 -0800

    cleaning

--- src/DSL/Schemas.hs
+++ src/DSL/Schemas.hs
@@ -1,22 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
 module DSL.Schemas where
 
-import DSL.Effect
-import DSL.Environment
--- -- import DSL.Expression
-import DSL.Model
-import DSL.Parser
-import DSL.Pretty
--- -- import DSL.Primitive
-import DSL.Profile
-import DSL.Resource
-
-import DSL.Serialize
 import qualified Data.Aeson             as A
 import           Data.JSON.Schema
-import           Data.JSON.Schema.Validate
 
 -- Sugar
 string = Value unboundedLength
