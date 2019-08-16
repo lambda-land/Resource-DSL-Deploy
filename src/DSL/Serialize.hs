5e5f772850936a711d520d1a96f1b5f5cdc0a8f6
Eric Walkingshaw <eric@walkingshaw.net>
2019-08-15 18:22:38 -0700

    Map unit value to JSON null.

--- src/DSL/Serialize.hs
+++ src/DSL/Serialize.hs
@@ -167,7 +167,7 @@ asPType = do
       _ -> throwCustomError (BadCase "primitive type" ["unit","bool","int","float","symbol"] t)
 
 instance ToJSON PVal where
-  toJSON Unit  = String "()"
+  toJSON Unit  = Null
   toJSON (B b) = Bool b
   toJSON (I i) = Number (fromInteger (toInteger i))
   toJSON (F d) = Number (fromFloatDigits d)
@@ -184,9 +184,9 @@ asPVal :: ParseIt PVal
 asPVal = do
     v <- asValue
     case v of
+      Null     -> pure Unit
       Bool b   -> pure (B b)
       Number n -> pure (either F I (floatingOrInteger n))
-      String "()" -> pure Unit
       String _ -> S <$> asSymbol
       _ -> throwCustomError (BadPVal v)
 
