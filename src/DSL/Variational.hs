8d27426b48d050b8418732ef6fd8395f26f931d8
Eric Walkingshaw <eric@walkingshaw.net>
2019-09-22 16:04:59 -0700

    Update variational type class.

--- src/DSL/Variational.hs
+++ src/DSL/Variational.hs
@@ -1,11 +1,8 @@
-module DSL.Preparation where
+module DSL.Variational where
 
 import Data.Set (Set)
 
-import DSL.Boolean
-import DSL.Evaluation
 import DSL.Condition
-import DSL.SAT
 import DSL.Types
 
 
@@ -13,20 +10,18 @@ import DSL.Types
 -- * Variational type class
 --
 
--- | Captures all kinds of objects that may contain variation. Note that the
---   'configure' and 'select' operations are quite expensive since the current
---   instances use the inefficient SAT interface in DSL.SAT.
+-- | Captures all kinds of objects that contain choices somewhere within them.
 class Variational a where
   
-  -- | Fully configure an object according to the given partial configuration.
-  --   The first alternative is chosen if it is consistent with the partial
-  --   configuration, otherwise the second is chosen.
-  configure :: BExpr -> a -> a
-
-  -- | Eliminate all choices in a term that are determined by a partial
-  --   configuration expressed as a boolean expression.
-  select :: BExpr -> a -> a
-
+  -- | Fully configure a variational value by evaluating each choice condition
+  --   to either true or false. Undefined boolean dimensions are assumed to be
+  --   false, undefined integer dimensions are assumed to be zero.
+  configure :: Env Var Bool -> Env Var Int -> a -> a
+  
+  -- | Partially configure a variational value by eliminating choices whose
+  --   conditions reduce to a boolean value in the given environments.
+  reduce :: Env Var Bool -> Env Var Int -> a -> a
+  
   -- | Get all boolean variables within choice conditions.
   boolDims :: a -> Set Var
 
@@ -39,81 +34,85 @@ class Variational a where
 -- Trivial instances
 
 instance Variational PVal where
-  configure _ = id
-  select    _ = id
-  boolDims    = mempty
-  intDims     = mempty
+  configure _ _ = id
+  reduce    _ _ = id
+  boolDims _ = mempty
+  intDims  _ = mempty
 
 instance Variational PType where
-  configure _ = id
-  select    _ = id
-  boolDims    = mempty
-  intDims     = mempty
+  configure _ _ = id
+  reduce    _ _ = id
+  boolDims _ = mempty
+  intDims  _ = mempty
 
 instance Variational Error where
-  configure _ = id
-  select    _ = id
-  boolDims    = mempty
-  intDims     = mempty
+  configure _ _ = id
+  reduce    _ _ = id
+  boolDims _ = mempty
+  intDims  _ = mempty
 
 
 -- Reduction instances
 
 instance Variational a => Variational (V a) where
-  configure c (One a) = One (configure c a)
-  configure c (Chc d l r)
-      | sat (c &&& d) = configure c l
-      | otherwise     = configure c r
+  configure mb mi (One a) = One (configure mb mi a)
+  configure mb mi (Chc d l r)
+      | evalBExpr mb mi (condExpr d) = configure mb mi l
+      | otherwise                    = configure mb mi r
 
-  select c (One a) = One (select c a)
-  select c (Chc d l r)
-      | implies  c d = select c l
-      | nimplies c d = select c r
-      | otherwise    = Chc d (select c l) (select c r)
+  reduce mb mi (One a) = One (reduce mb mi a)
+  reduce mb mi (Chc (Cond e s) l r) =
+      case reduceBExpr mb mi e of
+        BLit True  -> l'
+        BLit False -> r'
+        e' -> Chc (Cond e' s) l' r'
+    where
+      l' = reduce mb mi l
+      r' = reduce mb mi r
 
   boolDims (One _)     = mempty
-  boolDims (Chc d l r) = boolVars d <> boolDims l <> boolDims r
+  boolDims (Chc d l r) = boolVars (condExpr d) <> boolDims l <> boolDims r
 
   intDims (One _)     = mempty
-  intDims (Chc d l r) = intVars d <> intDims l <> intDims r
+  intDims (Chc d l r) = intVars (condExpr d) <> intDims l <> intDims r
 
 
 -- Congruence instances
 
 instance Variational a => Variational (Maybe a) where
-  configure c = fmap (configure c)
-  select    c = fmap (select c)
-  boolDims    = foldMap boolDims
-  intDims     = foldMap intDims
+  configure mb mi = fmap (configure mb mi)
+  reduce    mb mi = fmap (reduce mb mi)
+  boolDims = foldMap boolDims
+  intDims  = foldMap intDims
 
 instance Variational a => Variational [a] where
-  configure c = map (configure c)
-  select    c = map (select c)
-  boolDims    = foldMap boolDims
-  intDims     = foldMap intDims
+  configure mb mi = map (configure mb mi)
+  reduce    mb mi = map (reduce mb mi)
+  boolDims = foldMap boolDims
+  intDims  = foldMap intDims
 
 instance Variational v => Variational (Env k v) where
-  configure c = fmap (configure c)
-  select    c = fmap (select c)
-  boolDims    = foldMap boolDims
-  intDims     = foldMap intDims
+  configure mb mi = fmap (configure mb mi)
+  reduce    mb mi = fmap (reduce mb mi)
+  boolDims = foldMap boolDims
+  intDims  = foldMap intDims
 
 instance Variational Fun where
-  configure c (Fun ps e) = Fun ps (configure c e)
-  select    c (Fun ps e) = Fun ps (select c e)
-  boolDims    (Fun _  e) = boolDims e
-  intDims     (Fun _  e) = intDims e
+  configure mb mi (Fun ps e) = Fun ps (configure mb mi e)
+  reduce    mb mi (Fun ps e) = Fun ps (reduce mb mi e)
+  boolDims (Fun _  e) = boolDims e
+  intDims  (Fun _  e) = intDims e
 
 instance Variational Expr where
-  configure c (P1 o e)        = P1 o (configure c e)
-  configure c (P2 o e1 e2)    = P2 o (configure c e1) (configure c e2)
-  configure c (P3 o e1 e2 e3) = P3 o (configure c e1) (configure c e2) (configure c e3)
-  configure _ e = e
+  configure mb mi (P1 o e)        = P1 o (configure mb mi e)
+  configure mb mi (P2 o e1 e2)    = P2 o (configure mb mi e1) (configure mb mi e2)
+  configure mb mi (P3 o e1 e2 e3) = P3 o (configure mb mi e1) (configure mb mi e2) (configure mb mi e3)
+  configure _ _ e = e
   
-  select c (P1 o e)        = P1 o (select c e)
-  select c (P2 o e1 e2)    = P2 o (select c e1) (select c e2)
-  select c (P3 o e1 e2 e3) = P3 o (select c e1) (select c e2) (select c e3)
-  select _ e = e
+  reduce mb mi (P1 o e)        = P1 o (reduce mb mi e)
+  reduce mb mi (P2 o e1 e2)    = P2 o (reduce mb mi e1) (reduce mb mi e2)
+  reduce mb mi (P3 o e1 e2 e3) = P3 o (reduce mb mi e1) (reduce mb mi e2) (reduce mb mi e3)
+  reduce _ _ e = e
 
   boolDims (P1 _ e)        = boolDims e
   boolDims (P2 _ e1 e2)    = boolDims e1 <> boolDims e2
@@ -126,15 +125,15 @@ instance Variational Expr where
   intDims _ = mempty
 
 instance Variational Effect where
-  configure c (Create e) = Create (configure c e)
-  configure c (Check  f) = Check  (configure c f)
-  configure c (Modify f) = Modify (configure c f)
-  configure _ Delete     = Delete
+  configure mb mi (Create e) = Create (configure mb mi e)
+  configure mb mi (Check  f) = Check  (configure mb mi f)
+  configure mb mi (Modify f) = Modify (configure mb mi f)
+  configure _ _ Delete     = Delete
   
-  select c (Create e) = Create (select c e)
-  select c (Check  f) = Check  (select c f)
-  select c (Modify f) = Modify (select c f)
-  select _ Delete     = Delete
+  reduce mb mi (Create e) = Create (reduce mb mi e)
+  reduce mb mi (Check  f) = Check  (reduce mb mi f)
+  reduce mb mi (Modify f) = Modify (reduce mb mi f)
+  reduce _ _ Delete     = Delete
 
   boolDims (Create e) = boolDims e
   boolDims (Check  f) = boolDims f
@@ -147,38 +146,32 @@ instance Variational Effect where
   intDims Delete     = mempty
 
 instance Variational Stmt where
-  configure c (Do p e)     = Do p (configure c e)
-  configure c (If b t e)   = If (configure c b) (configure c t) (configure c e)
-  configure c (In p ss)    = In p (configure c ss)
-  configure c (Let v e ss) = Let v (configure c e) (configure c ss)
-  configure c (Load e es)  = Load (configure c e) (map (configure c) es)
+  configure mb mi (Do p e)     = Do p (configure mb mi e)
+  configure mb mi (If b t e)   = If (configure mb mi b) (configure mb mi t) (configure mb mi e)
+  configure mb mi (In p ss)    = In p (configure mb mi ss)
+  configure mb mi (Let v e ss) = Let v (configure mb mi e) (configure mb mi ss)
+  configure mb mi (Load e es)  = Load (configure mb mi e) (map (configure mb mi) es)
   
-  select c (Do p e)     = Do p (select c e)
-  select c (If b t e)   = If (select c b) (select c t) (select c e)
-  select c (In p ss)    = In p (select c ss)
-  select c (Let v e ss) = Let v (select c e) (select c ss)
-  select c (Load e es)  = Load (select c e) (map (select c) es)
+  reduce mb mi (Do p e)     = Do p (reduce mb mi e)
+  reduce mb mi (If b t e)   = If (reduce mb mi b) (reduce mb mi t) (reduce mb mi e)
+  reduce mb mi (In p ss)    = In p (reduce mb mi ss)
+  reduce mb mi (Let v e ss) = Let v (reduce mb mi e) (reduce mb mi ss)
+  reduce mb mi (Load e es)  = Load (reduce mb mi e) (map (reduce mb mi) es)
 
   boolDims (Do _ e)     = boolDims e
   boolDims (If b t e)   = boolDims b <> boolDims t <> boolDims e
   boolDims (In _ ss)    = boolDims ss
   boolDims (Let _ e ss) = boolDims e <> boolDims ss
-  boolDims (Load e es)  = boolDims e <> foldMap boolDims es
+  boolDims (Load e es)  = boolDims e <> boolDims es
 
   intDims (Do _ e)     = intDims e
   intDims (If b t e)   = intDims b <> intDims t <> intDims e
   intDims (In _ ss)    = intDims ss
   intDims (Let _ e ss) = intDims e <> intDims ss
-  intDims (Load e es)  = intDims e <> foldMap intDims es
+  intDims (Load e es)  = intDims e <> intDims es
 
 instance Variational Model where
-  configure c (Model ps ss) = Model ps (configure c ss)
-  select    c (Model ps ss) = Model ps (select c ss)
-  boolDims    (Model _  ss) = boolDims ss
-  intDims     (Model _  ss) = intDims ss
-
-instance Variational StateCtx where
-  configure c (SCtx r a e m) = SCtx (configure c r) a e (configure c m)
-  select    c (SCtx r a e m) = SCtx (select c r) a e (select c m)
-  boolDims    (SCtx r _ _ m) = boolDims r <> boolDims m
-  intDims     (SCtx r _ _ m) = intDims r <> intDims m
+  configure mb mi (Model ps ss) = Model ps (configure mb mi ss)
+  reduce    mb mi (Model ps ss) = Model ps (reduce mb mi ss)
+  boolDims (Model _  ss) = boolDims ss
+  intDims  (Model _  ss) = intDims ss
